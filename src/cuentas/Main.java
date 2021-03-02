991e39cb75402ebd48cf53ad094ab80020f55b64
Fran <Fran@AiKiDoPc>
2021-03-02 18:39:02 +0100

    2

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,17 +1,21 @@
 package cuentas;
 
-
-import cuentas.CCuenta;
-
 public class Main {
 
     public static void main(String[] args) {
+        operativa_cuenta();
+    }
+/**
+ * Creamos el metodo operativa_cuenta
+ */
+    private static void operativa_cuenta() {
         CCuenta cuenta1;
         double saldoActual;
+        float cantidad;
 
-        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es" + saldoActual);
+        System.out.println("El saldo actual es"+ saldoActual );
 
         try {
             cuenta1.retirar(2300);
